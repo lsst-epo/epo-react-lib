4f415e94db439f83747b69daca04625d3bce33df
Alexandra Goff <agoff@lsst.org>
2022-06-02 10:10:02 -0700

    Develop (#6)
    * [F] add project aliases
    
    * [F] set up Storybook and example component
    
    * [C] set up Storybook deploy
    
    * [F] set up Jest

--- dist/components/Atoms/Button/index.d.ts
+++ dist/components/Atoms/Button/index.d.ts
@@ -4,7 +4,8 @@ export interface Props extends HTMLAttributes<HTMLButtonElement> {
     className?: string;
     isInactive?: boolean;
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    styleAs: 'primary' | 'secondary';
+    styleAs?: 'primary' | 'secondary';
+
 }
 /**
  * A styled wrapper for the \<button> element
