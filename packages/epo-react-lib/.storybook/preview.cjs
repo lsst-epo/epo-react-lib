2e5face4954fac9c585d4fdf6481397206f34c9a
Alexandra Goff <agoff@lsst.org>
2022-09-23 10:41:07 -0700

    [F] port globalStyles from rubin-obs-client

--- packages/epo-react-lib/.storybook/preview.cjs
+++
@@ -1,8 +0,0 @@
-import { withTests } from "@storybook/addon-jest";
-import results from "../.jest-test-results.json";
-
-export const decorators = [
-  withTests({
-    results,
-  }),
-];
--- packages/epo-react-lib/src/components/DemoButton/DemoButton.tsx
+++ packages/epo-react-lib/src/components/DemoButton/DemoButton.tsx
@@ -3,17 +3,17 @@ import styled from "styled-components";
 const DemoButton = styled.button`
   border: none;
   border-radius: 0.5rem;
-  background-color: #186faf;
+  background-color: var(--turquoise80, black);
   color: hsl(0deg, 0%, 98%);
   padding: 0.75rem;
   cursor: pointer;
   &:hover {
-    background-color: #0a558c;
+    background-color: var(--turquoise70);
   }
   &:focus {
     outline: none;
     box-shadow: 0 0 0 2px #62b0e8;
-    background-color: #0a558c;
+    background-color: var(--turquoise70);
   }
 `;
 
