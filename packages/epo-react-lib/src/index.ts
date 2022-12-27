4439a0f3110be37ca8c1bcd729865b368f3bcc85
Alexandra Goff <agoff@lsst.org>
2022-12-27 15:21:11 -0700

    [F] add SimpleTable and ComplexTable

--- packages/epo-react-lib/src/index.ts
+++ packages/epo-react-lib/src/index.ts
@@ -1,29 +1,43 @@
+// styled-components
+
 export { default as GlobalStyles } from "@/styles/globalStyles";
+
+// i18n
 export { default as localeStrings } from "@/assets/locales";
 
+// Atomic
 export { default as Accordion } from "@/atomic/Accordion";
+export { default as Button } from "@/atomic/Button";
+export { default as Buttonish } from "@/atomic/Buttonish";
+export { default as ExpandToggle } from "@/atomic/ExpandToggle";
 export { default as ExternalLink } from "@/atomic/ExternalLink";
-export { default as Link } from "@/atomic/Link";
 export { default as Figure } from "@/atomic/Figure";
 export { default as Image } from "@/atomic/Image";
+export { default as Link } from "@/atomic/Link";
+export { default as MixedLink } from "@/atomic/MixedLink";
 export { default as ResponsiveImage } from "@/atomic/ResponsiveImage";
-export { default as IconComposer } from "@/svg/IconComposer";
-export { default as icons } from "@/svg/icons";
-export { default as Button } from "@/atomic/Button";
-export { default as ExpandToggle } from "@/atomic/Button";
+export * from "@/atomic/Share";
 export { default as Video } from "@/atomic/Video";
-export { default as CarouselLayout } from "@/layout/Carousel";
+
+// Content Blocks
+export { default as SimpleTable } from "@/content-blocks/SimpleTable";
+
+// Form
 export { default as Error } from "@/form/Error";
 export { default as FormButtons } from "@/form/FormButtons";
 export { default as FormField } from "@/form/FormField";
 export { default as Input, Password } from "@/form/Input";
 export { default as Select } from "@/form/Select";
 export { default as Switch } from "@/form/Switch";
-export { default as Buttonish } from "@/atomic/Buttonish";
-export { default as MixedLink } from "@/atomic/MixedLink";
-export * from "@/atomic/Share";
+
+// Layout
+export { default as BasicModal } from "@/layout/BasicModal";
+export { default as CarouselLayout } from "@/layout/Carousel";
 export { default as Columns } from "@/layout/Columns";
 export { default as Container } from "@/layout/Container";
 export { default as Grid } from "@/layout/Grid";
 export { default as MasonryGrid } from "@/layout/MasonryGrid";
-export { default as BasicModal } from "@/layout/BasicModal";
+
+// SVG
+export { default as IconComposer } from "@/svg/IconComposer";
+export { default as icons } from "@/svg/icons";
