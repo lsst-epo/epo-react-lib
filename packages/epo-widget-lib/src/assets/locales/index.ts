40250288ad3f3fd8a6e78928c9cc9502e80e91e0
Alexandra Goff <agoff@lsst.org>
2023-01-30 16:02:01 -0700

    [F] set up epo-widget-lib folder

---
+++ packages/epo-widget-lib/src/assets/locales/index.ts
@@ -0,0 +1,7 @@
+import en from "./en/translation.json";
+import es from "./es/translation.json";
+
+export default {
+  en,
+  es,
+};
---
+++ packages/epo-widget-lib/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [dts({ insertTypesEntry: true }), react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "EPO Widget Lib",
+      formats: ["es", "umd"],
+      fileName: (format) => `epo-widget-lib.${format}.js`,
+    },
+  },
+  resolve: {
+    alias: {
+      "@/assets": resolve(__dirname, "./src/assets"),
+    },
+  },
+});
