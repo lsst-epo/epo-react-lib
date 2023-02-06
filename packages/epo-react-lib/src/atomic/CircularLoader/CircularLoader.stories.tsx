c36954ec36f886dcbaa0b882a9943f7133ea32bd
Alexandra Goff <agoff@lsst.org>
2023-02-06 16:04:35 -0700

    [F] finish CircularLoader
    For JIRA: "EPO-7604 #IN-REVIEW #comment finish CircularLoader"

--- packages/epo-react-lib/src/atomic/CircularLoader/CircularLoader.stories.tsx
+++ packages/epo-react-lib/src/atomic/CircularLoader/CircularLoader.stories.tsx
@@ -1,14 +1,55 @@
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { speeds } from "./styles";
 
 import CircularLoader from ".";
+
+const speedKeys = Object.keys(speeds);
+const speedLabels: { [key: string]: string } = {};
+
+speedKeys.forEach((speed) => {
+  speedLabels[speed] = `${speed} - (${speeds[speed]})`;
+});
 
 const meta: ComponentMeta<typeof CircularLoader> = {
   component: CircularLoader,
   argTypes: {
-    fullScreen: { control: "boolean" },
-    isVisible: { control: "boolean" },
+    withOverlay: {
+      control: "boolean",
+      description:
+        "Displays the loader as a full screen with black background over top other content.",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: false },
+      },
+    },
+    isVisible: {
+      control: "boolean",
+      description: "Determines whether the loader is visible.",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: false },
+      },
+    },
+    speed: {
+      options: Object.keys(speeds),
+      control: { type: "select", labels: speedLabels },
+      description: "Speed that the loader spins.",
+      table: {
+        type: { summary: "slow | normal | fast" },
+        defaultValue: { summary: "normal" },
+      },
+    },
+    description: {
+      control: { type: "text" },
+      description:
+        "Status text shown to screenreaders when the loader becomes active. Used to indicate a loading message.",
+      table: {
+        type: { summary: "string" },
+      },
+    },
   },
 };
+
 export default meta;
 
 export const Primary: ComponentStoryObj<typeof CircularLoader> = {
