c3af9add9f7920377013e7b6281d6e68d257a0f8
Alexandra Goff <agoff@lsst.org>
2023-02-17 12:50:14 -0700

    [C] add Jest todos

---
+++ packages/epo-react-lib/src/form/SelectListbox/SelectListbox.test.tsx
@@ -0,0 +1,14 @@
+import SelectListbox from ".";
+
+describe("SelectListbox", () => {
+  it.todo("Should display a placeholder if no value is select");
+  it.todo("Should disable selection based on props");
+  it.todo("Should open a dropdown list if clicked");
+  it.todo("Should navigate the dropdown list options with arrow keys");
+  it.todo(
+    "Should close the dropdown list on selection, escape key, and click outside"
+  );
+  it.todo(
+    "Should select an option on click, enter key, and space key and update the display"
+  );
+});
