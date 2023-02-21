26416ead927aab07961d42f4f583983e1ea60faf
Alexandra Goff <agoff@lsst.org>
2023-02-21 14:08:03 -0700

    [C] move mock options to test

--- packages/epo-react-lib/src/form/SelectListbox/SelectListbox.stories.tsx
+++ packages/epo-react-lib/src/form/SelectListbox/SelectListbox.stories.tsx
@@ -3,48 +3,9 @@ import {
   ComponentStory,
   ComponentStoryObj,
 } from "@storybook/react";
-import ColorSwatch from "@/atomic/ColorSwatch";
-
+import { options } from "./SelectListbox.test";
 import SelectListbox from ".";
 import { useState } from "react";
-
-export const options = [
-  {
-    value: "red",
-    label: "Red",
-    icon: <ColorSwatch color="#D93831" size="small" />,
-  },
-  {
-    value: "orange",
-    label: "Orange",
-    icon: <ColorSwatch color="#E38E3E" size="small" />,
-  },
-  {
-    value: "yellow",
-    label: "Yellow",
-    icon: <ColorSwatch color="#F9DE4E" size="small" />,
-  },
-  {
-    value: "green",
-    label: "Green",
-    icon: <ColorSwatch color="#87D665" size="small" />,
-  },
-  {
-    value: "blue",
-    label: "Blue",
-    icon: <ColorSwatch color="#0618F5" size="small" />,
-  },
-  {
-    value: "violet",
-    label: "Violet",
-    icon: <ColorSwatch color="#7B26F5" size="small" />,
-  },
-  {
-    value: "none",
-    label: "None",
-    icon: <ColorSwatch color="transparent" size="small" />,
-  },
-];
 
 const meta: ComponentMeta<typeof SelectListbox> = {
   component: SelectListbox,
