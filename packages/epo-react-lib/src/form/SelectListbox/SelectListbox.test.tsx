da7b9574fd3ecf4887fff2e32db4573e580223aa
Alexandra Goff <agoff@lsst.org>
2023-02-21 14:08:03 -0700

    [C] move mock options to test

--- packages/epo-react-lib/src/form/SelectListbox/SelectListbox.test.tsx
+++ packages/epo-react-lib/src/form/SelectListbox/SelectListbox.test.tsx
@@ -1,8 +1,46 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { options } from "./SelectListbox.stories";
 import SelectListbox from ".";
 import { act } from "react-dom/test-utils";
+import ColorSwatch from "@/atomic/ColorSwatch";
+
+export const options = [
+  {
+    value: "red",
+    label: "Red",
+    icon: <ColorSwatch color="#D93831" size="small" />,
+  },
+  {
+    value: "orange",
+    label: "Orange",
+    icon: <ColorSwatch color="#E38E3E" size="small" />,
+  },
+  {
+    value: "yellow",
+    label: "Yellow",
+    icon: <ColorSwatch color="#F9DE4E" size="small" />,
+  },
+  {
+    value: "green",
+    label: "Green",
+    icon: <ColorSwatch color="#87D665" size="small" />,
+  },
+  {
+    value: "blue",
+    label: "Blue",
+    icon: <ColorSwatch color="#0618F5" size="small" />,
+  },
+  {
+    value: "violet",
+    label: "Violet",
+    icon: <ColorSwatch color="#7B26F5" size="small" />,
+  },
+  {
+    value: "none",
+    label: "None",
+    icon: <ColorSwatch color="transparent" size="small" />,
+  },
+];
 
 const props = {
   placeholder: "Select a value",
