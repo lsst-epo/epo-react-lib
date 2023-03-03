e1e0b369532712602822a0b5c1e2c99cc755e756
Alexandra Goff <agoff@lsst.org>
2023-03-03 14:29:08 -0700

    [F] add Toast and Message
    For JIRA: "EPO-7749 #IN-REVIEW #comment add Toast and Message"

---
+++ packages/epo-widget-lib/src/widgets/GalaxySelector/Message/Message.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Message from ".";
+
+const props = {
+  children: "My message",
+  onMessageChangeCallback: jest.fn(),
+  isVisible: false,
+};
+
+describe("Message", () => {
+  it(`should be visible if isVisible is set`, () => {
+    const { rerender } = render(<Message {...props} />);
+
+    const message = screen.getByRole("status");
+    const style = getComputedStyle(message);
+
+    expect(message).toBeInTheDocument();
+    expect(style.height).toBe("0px");
+
+    rerender(<Message {...{ ...props, isVisible: true }} />);
+
+    const rerenderedSyle = getComputedStyle(message);
+
+    expect(rerenderedSyle.height).not.toBe("0px");
+  });
+});
---
+++ packages/epo-widget-lib/src/widgets/GalaxySelector/Message/Message.tsx
@@ -0,0 +1,36 @@
+import { FunctionComponent, ReactNode, useEffect } from "react";
+import * as Styled from "./styles";
+
+interface MessageProps {
+  children?: ReactNode;
+  isVisible?: boolean;
+  onMessageChangeCallback: () => void;
+  /** list of ID's that contributed to this status message eg. if clicking on \<svg id="mySvg"/> created a message, pass ['mySvg'] */
+  forIds?: string[];
+}
+
+const Message: FunctionComponent<MessageProps> = ({
+  children,
+  isVisible = false,
+  onMessageChangeCallback,
+  forIds,
+}) => {
+  useEffect(() => {
+    if (isVisible) {
+      const timer = setTimeout(() => {
+        onMessageChangeCallback && onMessageChangeCallback();
+      }, 1800);
+      return () => clearTimeout(timer);
+    }
+  }, [children, isVisible]);
+
+  return (
+    <Styled.Message {...{ forIds }} isOpen={isVisible}>
+      {children}
+    </Styled.Message>
+  );
+};
+
+Message.displayName = "Widgets.GalaxySelector.Message";
+
+export default Message;
