f09dd8d90e8b96dcae6cd992e4c9e37fa1cefc99
Alexandra Goff <agoff@lsst.org>
2023-03-03 14:29:08 -0700

    [F] add Toast and Message
    For JIRA: "EPO-7749 #IN-REVIEW #comment add Toast and Message"

---
+++ packages/epo-widget-lib/src/widgets/GalaxySelector/Message/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Toast } from "@rubin-epo/epo-react-lib";
+
+export const Message = styled(Toast)<{ isOpen: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1ch;
+  width: 100%;
+  height: ${({ isOpen }) => (isOpen ? 24 : 0)}px;
+  overflow: hidden;
+  background-color: var(--black, #000);
+  color: #30e0e3;
+  font-size: 1rem;
+  font-weight: var(--FONT_WEIGHT_BOLD, 600);
+  text-align: center;
+  transition: height ease var(--DURATION_SLOW, 0.4s);
+
+  @media (prefers-reduced-motion) {
+    transition: unset;
+  }
+`;
