e6e46db519c532c73fe36698fa7bc8e2314bf96a
Alexandra Goff <agoff@lsst.org>
2023-03-17 14:17:50 -0700

    [C] fix tests not showing in storybook

--- packages/epo-widget-lib/.storybook/decorators.tsx
+++ packages/epo-widget-lib/.storybook/decorators.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { DecoratorFn } from "@storybook/react";
 import { withTests } from "@storybook/addon-jest";
+import results from "../.jest-test-results.json";
 import { GlobalStyles } from "@rubin-epo/epo-react-lib";
 
 const withTheme: DecoratorFn = (StoryFn) => {
@@ -13,4 +14,9 @@ const withTheme: DecoratorFn = (StoryFn) => {
 };
 
 // export all decorators that should be globally applied in an array
-export const globalDecorators = [withTheme];
+export const globalDecorators = [
+  withTheme,
+  withTests({
+    results,
+  }),
+];
