46902727b4e2c60115b1cc1c51199a74e2824745
Alexandra Goff <agoff@lsst.org>
2023-04-10 16:39:04 -0700

    [C] fix svg attrs

--- packages/epo-react-lib/src/svg/icons/Expand.tsx
+++ packages/epo-react-lib/src/svg/icons/Expand.tsx
@@ -22,8 +22,8 @@ const Expand: FunctionComponent<SVGProps> = ({
       <path
         d="M10.94,16V13.538h2.6v-2.6H16V16ZM0,16V10.94H2.462v2.6h2.6V16ZM13.538,5.06v-2.6h-2.6V0H16V5.06ZM0,5.06V0H5.06V2.462h-2.6v2.6Z"
         transform="translate(0.5 0.5)"
-        stroke-miterlimit="10"
-        stroke-width="1"
+        strokeMiterlimit={10}
+        strokeWidth={1}
       />
     </svg>
   );
