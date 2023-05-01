80f25868baf40f9dd2eed1fc6b530c4d6583bc3a
Alexandra Goff <agoff@lsst.org>
2023-05-01 13:31:49 -0700

    [F] ProgressBar

---
+++ packages/epo-react-lib/src/atomic/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,50 @@
+import { FunctionComponent, PropsWithChildren } from "react";
+import * as Styled from "./styles";
+
+interface ProgressBarProps {
+  min?: number;
+  max?: number;
+  value: number;
+  displayValue?: string;
+  labelledById?: string;
+  describedById?: string;
+  className?: string;
+}
+
+const ProgressBar: FunctionComponent<PropsWithChildren<ProgressBarProps>> = ({
+  min = 0,
+  max = 100,
+  value,
+  displayValue,
+  labelledById,
+  describedById,
+  children,
+  className,
+}) => {
+  const safeValue = Math.min(Math.max(value, min), max);
+  const isIndeterminate = isNaN(safeValue);
+
+  return (
+    <Styled.ProgressBarContainer className={className}>
+      <Styled.ProgressBar
+        role="progressbar"
+        aria-valuemin={min}
+        aria-valuemax={max}
+        aria-valuenow={isIndeterminate ? undefined : safeValue}
+        aria-labelledby={labelledById}
+        aria-describedby={describedById}
+        aria-valuetext={displayValue}
+        $value={isIndeterminate ? undefined : safeValue}
+      >
+        {!isIndeterminate && (
+          <Styled.Marker $active $hoverable $value={safeValue}>
+            {displayValue || `${safeValue}%`}
+          </Styled.Marker>
+        )}
+        {children}
+      </Styled.ProgressBar>
+    </Styled.ProgressBarContainer>
+  );
+};
+
+export default ProgressBar;
