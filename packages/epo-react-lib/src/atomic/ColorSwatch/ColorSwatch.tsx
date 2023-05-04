a7022f9a9fe2b7bfccceff63e7baf83b6f72820a
Alexandra Goff <agoff@lsst.org>
2023-05-04 10:48:28 -0700

    [C] add className to ColorSwatch for styled-components

--- packages/epo-react-lib/src/atomic/ColorSwatch/ColorSwatch.tsx
+++ packages/epo-react-lib/src/atomic/ColorSwatch/ColorSwatch.tsx
@@ -3,19 +3,19 @@ import { ScreenreaderText } from "@/styles/utils";
 import * as Styled from "./styles";
 
 interface ColorSwatchProps {
-  classes?: string;
+  className?: string;
   content?: string;
   color: string;
   size?: "small" | "normal" | "large";
 }
 
 const ColorSwatch: FunctionComponent<ColorSwatchProps> = ({
-  classes,
+  className,
   content,
   color,
   size = "normal",
 }) => (
-  <span className={classes}>
+  <span className={className}>
     <Styled.ColorSwatch {...{ size, color }} data-testid="color-swatch" />
     <ScreenreaderText>{content || color}</ScreenreaderText>
   </span>
