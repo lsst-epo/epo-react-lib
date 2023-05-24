536d0014d0e7f61839394d4ea2113675d350edc4
Alexandra Goff <agoff@lsst.org>
2023-05-24 09:27:04 -0700

    [C] localeStrings to public directory

--- packages/epo-react-lib/.storybook/i18next.js
+++ packages/epo-react-lib/.storybook/i18next.js
@@ -1,22 +1,24 @@
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import resources from "../src/assets/locales";
 
 const supportedLngs = ["en", "es"];
-
+const ns = ["epo-react-lib"];
 i18n
+  .use(Backend)
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
     debug: true,
     lng: "en",
     fallbackLng: "en",
-    defaultNS: "translation",
+    ns,
+    defaultNS: "epo-react-lib",
     interpolation: { escapeValue: false },
     react: { useSuspense: false },
     supportedLngs,
-    resources,
+    // resources,
   });
 
 export default i18n;
