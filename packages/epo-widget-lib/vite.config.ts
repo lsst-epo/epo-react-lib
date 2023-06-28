af226e015f7c2f2b1bbb73a067b4921479d04f34
Alexandra Goff <agoff@lsst.org>
2023-06-28 10:58:58 -0700

    [C] begin updating epo-react-lib

--- packages/epo-widget-lib/vite.config.ts
+++ packages/epo-widget-lib/vite.config.ts
@@ -6,6 +6,7 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [dts({ insertTypesEntry: true }), react()],
+  root: "./",
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
