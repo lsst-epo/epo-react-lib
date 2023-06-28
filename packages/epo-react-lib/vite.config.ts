0a99fb4a7cfb453362603e25b6ba8d9958e698bf
Alexandra Goff <agoff@lsst.org>
2023-06-28 11:53:02 -0700

    [C] export *.d.ts, update styled-components

--- packages/epo-react-lib/vite.config.ts
+++ packages/epo-react-lib/vite.config.ts
@@ -5,7 +5,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ insertTypesEntry: true }), react()],
+  plugins: [dts({ insertTypesEntry: true, copyDtsFiles: true }), react()],
   root: "./",
   build: {
     lib: {
