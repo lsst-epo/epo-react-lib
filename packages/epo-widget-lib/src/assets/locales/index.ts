7274bdfcf22e7c31ca0c81d90e6813af342634d9
Alexandra Goff <agoff@lsst.org>
2023-06-28 10:58:58 -0700

    [C] begin updating epo-react-lib

--- packages/epo-widget-lib/src/assets/locales/index.ts
+++
@@ -1,8 +0,0 @@
-import { localeStrings } from "@rubin-epo/epo-react-lib";
-import en from "./en/translation.json";
-import es from "./es/translation.json";
-
-en.translation = { ...en.translation, ...localeStrings.en.translation };
-es.translation = { ...es.translation, ...localeStrings.es.translation };
-
-export default { en, es };
