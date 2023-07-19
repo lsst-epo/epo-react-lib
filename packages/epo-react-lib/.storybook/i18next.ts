271cd3c6396672073542e1f01239ffef3f415d39
Alexandra Goff <agoff@lsst.org>
2023-07-19 15:51:34 -0700

    [C] fix relative paths

--- packages/epo-react-lib/.storybook/i18next.ts
+++ packages/epo-react-lib/.storybook/i18next.ts
@@ -1,6 +1,6 @@
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
-import Backend from "i18next-http-backend";
+import Backend, { HttpBackendOptions } from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 const supportedLngs = ["en", "es"];
@@ -9,7 +9,8 @@ i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(Backend)
-  .init({
+  .init<HttpBackendOptions>({
+    backend: { loadPath: "./locales/{{lng}}/{{ns}}.json" },
     lng: "en",
     fallbackLng: "en",
     ns,
