36b7c76af901d568c30fb98004a2f29f5539813f
Alexandra Goff <agoff@lsst.org>
2023-12-18 17:01:50 -0700

    [F] style dictionary setup

---
+++ packages/epo-style-dictionary/dist/web/library/tokens.legacy.js
@@ -0,0 +1,38 @@
+/**
+ * Legacy library tokens
+ * Avoid using these
+ * 
+ * Do not edit directly
+ * Generated on Mon, 18 Dec 2023 23:58:25 GMT
+ */
+
+export const white = "#000000";
+export const offWhite = "#e9e9e9";
+export const neutral02 = "#fafafa";
+export const neutral08 = "#ebebeb";
+export const neutral10 = "#f5f5f5";
+export const neutral15 = "#e3e4e8";
+export const neutral20 = "#dce0e3";
+export const neutral30 = "#d3d4d5";
+export const neutral40 = "#9e9ea3";
+export const neutral60 = "#6a6e6e";
+export const neutral80 = "#404040";
+export const neutral90 = "#2b2e34";
+export const black = "#000000";
+export const turquoise05 = "#edfffe";
+export const turquoise07 = "#edfffe"; // Merged with turquoise05
+export const turquoise10 = "#d9f7f6";
+export const turquoise20 = "#b1f2ef";
+export const turquoise50 = "#00babc";
+export const turquoise55 = "#009fa1";
+export const turquoise60 = "#058b8c"; // Merged with turquoise70
+export const turquoise70 = "#058b8c";
+export const turquoise80 = "#117273";
+export const turquoise85 = "#12726d";
+export const turquoise90 = "#0c4a47";
+export const orange02 = "#fffdfa";
+export const orange04 = "#fff8f0";
+export const orange05 = "#fff8f0"; // Merged with orange04
+export const orange10 = "#ffedd9";
+export const orange20 = "#ffe7cc";
+export const orange55 = "#fab364";
