309b397f7492ea0a505c2ed702e97a56a7732b16
Alexandra Goff <agoff@lsst.org>
2023-12-20 11:11:47 -0700

    [C] respect transparency and motion preferences

--- packages/epo-react-lib/src/atomic/Slideout/styles.ts
+++ packages/epo-react-lib/src/atomic/Slideout/styles.ts
@@ -2,9 +2,12 @@ import { zStack } from "@/styles/abstracts";
 import styled from "styled-components";
 
 export const Transition = styled.div`
-  --slide-time: 400ms;
-  --slide-delay: 200ms;
-  --transition-time: calc(var(--slide-time) + var(--slide-delay));
+  --transition-time: calc(var(--slide-time, 0) + var(--slide-delay, 0));
+
+  @media (prefers-reduced-motion: no-preference) {
+    --slide-time: 400ms;
+    --slide-delay: 200ms;
+  }
 
   position: fixed;
   top: 0;
@@ -19,20 +22,24 @@ export const Overlay = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
-  transition: background-color var(--slide-time);
-  transition-delay: var(--slide-delay);
+  transition: background-color var(--slide-time, 0);
+  transition-delay: var(--slide-delay, 0);
 
   &.closed {
     --background-color: transparent;
   }
 
   &.open {
-    --background-color: rgba(0, 0, 0, 0.7);
+    --background-color: rgba(0, 0, 0, var(--overlay-opacity, 0.9));
+
+    @media (prefers-reduced-transparency: no-preference) {
+      --overlay-opacity: 0.7;
+    }
   }
 `;
 export const SlideoutContainer = styled.div`
   position: absolute;
-  transition: transform var(--slide-time);
+  transition: transform var(--slide-time, 0);
   &.closed {
     transform: var(--transform);
   }
