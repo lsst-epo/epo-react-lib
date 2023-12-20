41d9d3671d8dfc7f3b477712ec0547ffaf226a48
Alexandra Goff <agoff@lsst.org>
2023-12-20 10:15:19 -0700

    [C] add to React library

---
+++ packages/epo-style-dictionary/dist/web/legacy/palette/index.cjs
@@ -0,0 +1,46 @@
+/**
+ * Legacy library tokens
+ * Avoid using these
+ * 
+ * Do not edit directly
+ * Generated on Wed, 20 Dec 2023 17:10:03 GMT
+ */
+
+module.exports = {
+  "white": "#ffffff",
+  "offWhite": "#e9e9e9",
+  "neutral02": "#fafafa",
+  "neutral08": "#ebebeb",
+  "neutral10": "#f5f5f5",
+  "neutral15": "#e3e4e8",
+  "neutral20": "#dce0e3",
+  "neutral30": "#d3d4d5",
+  "neutral40": "#9e9ea3",
+  "neutral60": "#6a6e6e",
+  "neutral80": "#404040",
+  "neutral90": "#2b2e34",
+  "neutral95": "#1f2121",
+  "black": "#000000",
+  "turquoise05": "#edfffe",
+  "turquoise07": "#edfffe",
+  "turquoise10": "#d9f7f6",
+  "turquoise20": "#b1f2ef",
+  "turquoise50": "#00babc",
+  "turquoise55": "#009fa1",
+  "turquoise60": "#058b8c",
+  "turquoise70": "#058b8c",
+  "turquoise80": "#117273",
+  "turquoise85": "#12726d",
+  "turquoise90": "#0c4a47",
+  "orange02": "#fffdfa",
+  "orange04": "#fff8f0",
+  "orange05": "#fff8f0",
+  "orange10": "#ffedd9",
+  "orange20": "#ffe7cc",
+  "orange55": "#fab364",
+  "blue20": "#c7d4f4",
+  "green05": "#d7e6cf",
+  "red": "#cf4040",
+  "red20": "#f2c3c0",
+  "red40": "#ff8489"
+};
---
+++ packages/epo-style-dictionary/dist/web/legacy/tokens/index.cjs
@@ -0,0 +1,42 @@
+/**
+ * Legacy library tokens
+ * Avoid using these
+ * 
+ * Do not edit directly
+ * Generated on Wed, 20 Dec 2023 17:10:03 GMT
+ */
+
+module.exports = {
+  "FONT_WEIGHT_NORMAL": "400",
+  "FONT_WEIGHT_MEDIUM": "500",
+  "FONT_WEIGHT_BOLD": "600",
+  "FONT_SIZE_BASE_DESKTOP": "22px",
+  "FONT_SIZE_BASE_MOBILE": "16px",
+  "FONT_STACK_BASE": "'Source Sans Pro', 'Helvetica Neue', Helvetica, 'Arial Black', Arial, sans-serif, system-ui",
+  "BREAK_HEADER_LAYOUT": "1500px",
+  "BREAK_DESKTOP": "1280px",
+  "BREAK_DESKTOP_SMALL": "1130px",
+  "BREAK_LARGE_TABLET": "850px",
+  "BREAK_LARGE_TABLET_MIN": "851px",
+  "BREAK_TABLET": "768px",
+  "BREAK_TABLET_MIN": "769px",
+  "BREAK_PHABLET": "600px",
+  "BREAK_PHABLET_MIN": "601px",
+  "BREAK_MOBILE": "446px",
+  "BREAK_MOBILE_MIN": "280px",
+  "CONTAINER_MAX": "2560px",
+  "CONTAINER_FULL": "1920px",
+  "CONTAINER_WIDE": "1435px",
+  "CONTAINER_REGULAR": "1160px",
+  "CONTAINER_NARROW": "900px",
+  "LINE_HEIGHT_BASE": "1.636",
+  "PADDING_SMALL": "20px",
+  "PADDING_MEDIUM": "40px",
+  "PADDING_LARGE": "100px",
+  "DURATION": "0.2s",
+  "DURATION_SLOW": "0.4s",
+  "DURATION_SUPER_SLOW": "1s",
+  "Z_STACK_LOADER": 45,
+  "Z_STACK_DIALOG": 35,
+  "Z_STACK_HEADER": 25
+};
