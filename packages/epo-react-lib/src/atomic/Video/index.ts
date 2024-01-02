994087f4a70a1f58c1d792f18cda1be968b5c721
Alexandra Goff <agoff@lsst.org>
2024-01-02 14:24:56 -0700

    [F] new generic video player

--- packages/epo-react-lib/src/atomic/Video/index.ts
+++ packages/epo-react-lib/src/atomic/Video/index.ts
@@ -1,1 +1,3 @@
-export { default } from "./Video";
+export { default } from "./patterns/YouTube";
+export { default as YouTubePlayer } from "./patterns/YouTube";
+export { default as GenericPlayer } from "./patterns/Generic";
