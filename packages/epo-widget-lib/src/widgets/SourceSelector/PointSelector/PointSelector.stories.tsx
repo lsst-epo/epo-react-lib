6c7999413f4abd815c7699fa2808b7c277644ddc
Alexandra Goff <agoff@lsst.org>
2024-01-23 10:45:13 -0700

    [C] source selector refactors

---
+++ packages/epo-widget-lib/src/widgets/SourceSelector/PointSelector/PointSelector.stories.tsx
@@ -0,0 +1,89 @@
+import { SourceDataset } from "@/types/astro";
+import { Meta, StoryObj } from "@storybook/react";
+
+import PointSelector from ".";
+
+const width = 600;
+const height = 600;
+const data: SourceDataset = {
+  name: "All Three Filters",
+  id: "ZTF19abvhduf",
+  color: "#fed828",
+  distance: 124.63102831794073,
+  dec: 56.07525,
+  velocity: 8573.901530035577,
+  ra: 97.139708,
+  redshift: 0.029,
+  alerts: [],
+  sources: [
+    {
+      id: "01",
+      type: "galaxy",
+      x: 120,
+      y: 390,
+      color: "#fed828",
+    },
+    {
+      id: "02",
+      type: "galaxyFilter",
+      x: 295,
+      y: 315,
+      color: "#fed828",
+    },
+    {
+      id: "03",
+      type: "supernova",
+      x: 495,
+      y: 220,
+      color: "#fed828",
+    },
+  ],
+};
+
+const meta: Meta<typeof PointSelector> = {
+  argTypes: {
+    sources: {
+      control: "object",
+      description: "Array of sources to be rendered as points.",
+      table: {
+        type: {
+          summary: "Source[]",
+        },
+        category: "Model",
+      },
+    },
+    selectedSource: {
+      control: "object",
+      description:
+        "ID of the currently selected source that will be visible to the user.",
+      table: {
+        type: {
+          summary: "string[]",
+        },
+        category: "Model",
+      },
+    },
+    onSelectCallback: {
+      action: "Selected source",
+      description:
+        "Returns the ID of the source if clicked, otherwise returns undefined",
+      table: {
+        type: {
+          summary: "(id?: string) => void",
+        },
+        category: "Function",
+      },
+    },
+  },
+  component: PointSelector,
+};
+export default meta;
+
+export const Primary: StoryObj<typeof PointSelector> = {
+  args: {
+    sources: data.sources,
+    selectedSource: [data.sources[0].id],
+    width,
+    height,
+  },
+};
