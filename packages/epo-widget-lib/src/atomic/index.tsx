ef755dee5190d0b71258420a5c0b6c405bddefee
Alexandra Goff <agoff@lsst.org>
2024-01-25 14:23:44 -0700

    [F] image stacker

--- packages/epo-widget-lib/src/atomic/index.tsx
+++ packages/epo-widget-lib/src/atomic/index.tsx
@@ -1,3 +1,4 @@
 export { default as Blinker } from "./Blinker";
 export { default as ElapsedTime } from "./ElapsedTime";
 export { default as PlaybackControl } from "./PlaybackControl";
+export { default as ImageStack } from "./ImageStack";
