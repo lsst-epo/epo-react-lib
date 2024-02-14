9a21cb3be3b31efd498dc1bc0b78d1ac81c5925c
Alexandra Goff <agoff@lsst.org>
2024-02-14 13:41:28 -0700

    [C] finish base plot

--- packages/epo-widget-lib/src/widgets/LightCurveTool/Plot/index.tsx
+++ packages/epo-widget-lib/src/widgets/LightCurveTool/Plot/index.tsx
@@ -1,9 +1,12 @@
-import { FunctionComponent } from "react";
-import { max, min, nice, range, ticks } from "d3-array";
-import { scaleLinear } from "d3-scale";
-import { Base, XAxis, YAxis } from "@/charts/index";
+import { FunctionComponent, PropsWithChildren, useState } from "react";
+import { useTranslation, Trans } from "react-i18next";
+import { max, min } from "d3-array";
+import useAxis from "@/charts/hooks/useAxis";
+import { Base, Tooltip, XAxis, YAxis } from "@/charts/index";
 import { Alert } from "@/types/astro";
 import { ChartMargin } from "@/charts/types";
+import { timestampFromMJD } from "@/lib/utils";
+import Point from "../Point";
 
 interface LightCurvePlotProps {
   alerts: Array<Alert>;
@@ -17,21 +20,34 @@ const daysSincePeak = (current: number, peak: number) => {
   return current - peak;
 };
 
-const LightCurvePlot: FunctionComponent<LightCurvePlotProps> = ({
-  alerts,
-  activeAlertId,
-  peakMjd,
-  yMin = 14,
-  yMax = 20,
-}) => {
-  const data = alerts.map(({ date, magnitude }) => {
-    return { x: daysSincePeak(date, peakMjd), y: magnitude };
+const LightCurvePlot: FunctionComponent<
+  PropsWithChildren<LightCurvePlotProps>
+> = ({ alerts, activeAlertId, peakMjd, yMin = 14, yMax = 20, children }) => {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+  const [hoveredIndex, setHovered] = useState<number>();
+  const data = alerts.map(({ date, magnitude, error, id }) => {
+    return {
+      x: daysSincePeak(date, peakMjd),
+      y: magnitude,
+      error,
+      magnitude,
+      id,
+      date,
+    };
   });
 
+  const activeItem =
+    typeof hoveredIndex !== "undefined"
+      ? data[hoveredIndex]
+      : data.find(({ id }) => id === activeAlertId);
+
   const margins: ChartMargin = {
-    top: 0,
-    left: 25,
-    right: 0,
+    top: 10,
+    left: 30,
+    right: 10,
     bottom: 20,
   };
   const width = 600;
@@ -39,41 +55,87 @@ const LightCurvePlot: FunctionComponent<LightCurvePlotProps> = ({
 
   const xMin = min(data, ({ x }) => x) || Math.min(...data.map(({ x }) => x));
   const xMax = max(data, ({ x }) => x) || Math.max(...data.map(({ x }) => x));
-  const xStep = 10;
-  const xTickCount = (Math.ceil((xMax - xMin + 1) / 10) * 10) / xStep;
-  const xDomain = nice(xMin, xMax, xTickCount);
-  const xTicks = range(xDomain[0], xDomain[1], xStep);
-  const xScale = scaleLinear(xDomain, [
-    0 + margins.left,
-    width - margins.right,
-  ]);
+
+  const [xDomain, xTicks, xScale] = useAxis({
+    min: xMin,
+    max: xMax,
+    step: 10,
+    range: [0 + margins.left, width - margins.right],
+  });
+
   const xRoot = xScale(xDomain[0]);
 
-  const yStep = 0.5;
-  const yTicks = range(yMin, yMax, yStep);
-  const yDomain = nice(yMin, yMax, yTicks.length);
-  const yScale = scaleLinear(yDomain, [
-    0 + margins.top,
-    height - margins.bottom,
-  ]);
+  const [yDomain, yTicks, yScale] = useAxis({
+    min: yMin,
+    max: yMax,
+    step: 0.5,
+    range: [0 + margins.top, height - margins.bottom],
+  });
+
   const yRoot = yScale(yDomain[1]);
 
+  const tooltipDateOptions: Intl.DateTimeFormatOptions = {
+    timeZone: "UTC",
+    dateStyle: "short",
+    timeStyle: "short",
+  };
+
   return (
-    <Base {...{ width, height }}>
+    <Base
+      {...{ width, height }}
+      horizontalLabel="Days"
+      verticalLabel="Apparent Magnitude (m)"
+    >
       <rect
         x={xScale(0)}
-        y={yScale(yDomain[1])}
+        y={yScale(yDomain[0])}
         width={xScale(15) - xScale(0)}
-        height={yScale(yDomain[0]) - yScale(yDomain[1])}
+        height={yRoot - yScale(yDomain[0])}
         fill="#F5F5F5"
       />
       <XAxis ticks={xTicks} y={yRoot} {...{ xDomain, xScale }} />
       <YAxis ticks={yTicks} x={xRoot} {...{ yDomain, yScale }} />
-      {data.map(({ x, y }) => (
-        <circle cx={xScale(x)} cy={yScale(y)} r="4">
-          {x}, {y}
-        </circle>
-      ))}
+      <g role="list" aria-label={t("light_curve.plot.plot_label") || undefined}>
+        {data.map(({ x, y, error, id }, i) => {
+          const days = Math.round(x);
+          const context = days > 0 ? "after" : days === 0 ? "peak" : "before";
+          return (
+            <Point
+              key={id}
+              x={xScale(x)}
+              y={yScale(y)}
+              error={yScale(error) - yScale(0)}
+              onMouseOver={() => setHovered(i)}
+              onMouseOut={() => setHovered(undefined)}
+              description={
+                t("light_curve.plot.point_label", {
+                  magnitude: y,
+                  count: Math.abs(days),
+                  context,
+                }) || undefined
+              }
+            />
+          );
+        })}
+      </g>
+      {children}
+      <Tooltip
+        x={activeItem ? xScale(activeItem.x) : undefined}
+        y={activeItem ? yScale(activeItem.y) : undefined}
+        visible={!!activeItem}
+        offset={6}
+      >
+        <Trans i18nKey="light_curve.plot.tooltip">
+          Apparent Magnitude: {{ magnitude: activeItem?.magnitude }}
+          <br />
+          Date:
+          {{
+            date: new Date(
+              timestampFromMJD(activeItem?.date || 0)
+            ).toLocaleString(language, tooltipDateOptions),
+          }}
+        </Trans>
+      </Tooltip>
     </Base>
   );
 };
