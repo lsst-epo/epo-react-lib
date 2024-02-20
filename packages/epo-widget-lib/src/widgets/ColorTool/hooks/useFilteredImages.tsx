af07449c3c8403de1466665ac30954269f649dd0
Alexandra Goff <agoff@lsst.org>
2024-02-20 10:55:06 -0700

    [C] return loaded status instead of callback

--- packages/epo-widget-lib/src/widgets/ColorTool/hooks/useFilteredImages.tsx
+++ packages/epo-widget-lib/src/widgets/ColorTool/hooks/useFilteredImages.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import isEqual from "lodash/isEqual";
 import { ImageShape } from "@rubin-epo/epo-react-lib/Image";
 import usePrevious from "@/hooks/usePrevious";
@@ -8,14 +8,12 @@ import { getFilters, updateColor } from "../lib/canvas";
 interface useFilteredImagesProps {
   images: Array<ImageShape>;
   filters: Array<ImageFilter>;
-  loadedCallback: () => void;
 }
 
 const useFilteredImages = ({
   images,
   filters,
-  loadedCallback,
-}: useFilteredImagesProps) => {
+}: useFilteredImagesProps): [Array<HTMLCanvasElement>, boolean] => {
   const previousImages = usePrevious(images);
   const [imagesLoaded, setImagesLoaded] = useState(0);
   const [imgElements, setImages] = useState<Array<HTMLImageElement>>();
@@ -40,36 +38,32 @@ const useFilteredImages = ({
     }
   }, [images]);
 
-  useEffect(() => {
-    if (loaded) {
-      loadedCallback();
-    }
-  }, [loaded]);
+  const canvases = [...(imgElements || [])]
+    .map((img, i) => {
+      if (!filters[i].active) return undefined;
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
 
-  const canvases = imgElements?.map((img, i) => {
-    if (!filters[i].active) return undefined;
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
+      if (ctx) {
+        const { width, height } = img;
+        const { color = "transparent", brightness } = filters[i];
 
-    if (ctx) {
-      const { width, height } = img;
-      const { color = "transparent", brightness } = filters[i];
+        ctx.canvas.width = width;
+        ctx.canvas.height = height;
+        ctx.clearRect(0, 0, width, height);
 
-      ctx.canvas.width = width;
-      ctx.canvas.height = height;
-      ctx.clearRect(0, 0, width, height);
+        ctx.filter = getFilters({ brightness, contrast: 1.3 });
 
-      ctx.filter = getFilters({ brightness, contrast: 1.3 });
+        ctx.drawImage(img, 0, 0, width, height);
 
-      ctx.drawImage(img, 0, 0, width, height);
+        updateColor(ctx, color, width, height);
+      }
 
-      updateColor(ctx, color, width, height);
-    }
+      return canvas;
+    })
+    .filter((img): img is HTMLCanvasElement => !!img);
 
-    return canvas;
-  });
-
-  return canvases;
+  return [canvases, !!loaded];
 };
 
 export default useFilteredImages;
