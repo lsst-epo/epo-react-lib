9fc25773ac38743898fdef7ec2af04f1dde35c54
Alexandra Goff <agoff@lsst.org>
2024-04-16 09:33:14 -0700

    [C] move Blinker controls to overlay

--- packages/epo-widget-lib/src/atomic/Blinker/styles.ts
+++ packages/epo-widget-lib/src/atomic/Blinker/styles.ts
@@ -1,23 +1,15 @@
 import styled from "styled-components";
 import Controls from "./Controls/Controls";
-import ImageStack from "../ImageStack";
 
 export const BlinkerContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr min-content;
-  justify-items: center;
+  aspect-ratio: 1;
+  display: flex;
+  justify-content: center;
   position: relative;
   height: 100%;
 `;
 
 export const BlinkerControls = styled(Controls)`
-  grid-row: 2;
-  margin-block-start: var(--PADDING_SMALL, 20px);
-`;
-
-export const BlinkerImages = styled(ImageStack)`
-  aspect-ratio: 1;
-  grid-row: 1;
-  width: 100%;
+  position: absolute;
+  bottom: var(--PADDING_SMALL, 20px);
 `;
