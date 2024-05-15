3c4b35f3073723c18731c6869b5cd64470cfa98d
Alexandra Goff <agoff@lsst.org>
2024-05-15 15:23:54 -0700

    [C] storybook deploy setup

---
+++ packages/epo-style-dictionary/dist/web/legacy/tokens/index.js
@@ -0,0 +1,40 @@
+/**
+ * Legacy library tokens
+ * Avoid using these
+ * 
+ * Do not edit directly
+ * Generated on Wed, 15 May 2024 19:05:35 GMT
+ */
+
+export const FONT_WEIGHT_NORMAL = "400";
+export const FONT_WEIGHT_MEDIUM = "500";
+export const FONT_WEIGHT_BOLD = "600";
+export const FONT_SIZE_BASE_DESKTOP = "22px";
+export const FONT_SIZE_BASE_MOBILE = "16px";
+export const FONT_STACK_BASE = "'Source Sans Pro', 'Helvetica Neue', Helvetica, 'Arial Black', Arial, sans-serif, system-ui";
+export const BREAK_HEADER_LAYOUT = "1500px";
+export const BREAK_DESKTOP = "1280px";
+export const BREAK_DESKTOP_SMALL = "1130px";
+export const BREAK_LARGE_TABLET = "850px";
+export const BREAK_LARGE_TABLET_MIN = "851px";
+export const BREAK_TABLET = "768px";
+export const BREAK_TABLET_MIN = "769px";
+export const BREAK_PHABLET = "600px";
+export const BREAK_PHABLET_MIN = "601px";
+export const BREAK_MOBILE = "446px";
+export const BREAK_MOBILE_MIN = "280px";
+export const CONTAINER_MAX = "2560px";
+export const CONTAINER_FULL = "1920px";
+export const CONTAINER_WIDE = "1435px";
+export const CONTAINER_REGULAR = "1160px";
+export const CONTAINER_NARROW = "900px";
+export const LINE_HEIGHT_BASE = "1.636";
+export const PADDING_SMALL = "20px";
+export const PADDING_MEDIUM = "40px";
+export const PADDING_LARGE = "100px";
+export const DURATION = "0.2s";
+export const DURATION_SLOW = "0.4s";
+export const DURATION_SUPER_SLOW = "1s";
+export const Z_STACK_LOADER = 45;
+export const Z_STACK_DIALOG = 35;
+export const Z_STACK_HEADER = 25;
