0db700e6c239c7edd92b43becb7221f445b15858
Alexandra Goff <agoff@lsst.org>
2024-06-13 15:16:20 -0700

    [F] set up Rubin brand theme

---
+++ packages/epo-style-dictionary/dist/web/rubin/index.d.ts
@@ -0,0 +1,125 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 13 Jun 2024 22:14:38 GMT
+ */
+
+export const COLOR_BLUE_13 : string;
+/** Rubin Blue */
+export const COLOR_BLUE_62 : string;
+export const COLOR_GREEN_14 : string;
+/** Rubin Green */
+export const COLOR_GREEN_54 : string;
+export const COLOR_NEUTRAL_10 : string;
+/** Rubin Light Gray */
+export const COLOR_NEUTRAL_12 : string;
+export const COLOR_NEUTRAL_17 : string;
+export const COLOR_NEUTRAL_37 : string;
+/** Rubin Gray */
+export const COLOR_NEUTRAL_58 : string;
+export const COLOR_NEUTRAL_75 : string;
+/** Rubin Charcoal */
+export const COLOR_NEUTRAL_80 : string;
+export const COLOR_NEUTRAL_81 : string;
+/** Rubin Black */
+export const COLOR_NEUTRAL_87 : string;
+export const COLOR_NEUTRAL_100 : string;
+export const COLOR_NEUTRAL_00 : string;
+export const COLOR_NEUTRAL_02 : string;
+/** Rubin Pale Gray */
+export const COLOR_NEUTRAL_04 : string;
+export const COLOR_NEUTRAL_08 : string;
+export const COLOR_NEUTRAL_09 : string;
+export const COLOR_ORANGE_10 : string;
+/** Rubin Orange */
+export const COLOR_ORANGE_31 : string;
+export const COLOR_ORANGE_01 : string;
+export const COLOR_ORANGE_03 : string;
+export const COLOR_ORANGE_07 : string;
+export const COLOR_BACKGROUND_PRIMARY : string;
+export const COLOR_BACKGROUND_INVERT : string;
+export const COLOR_PURPLE_67 : string;
+export const COLOR_RED_15 : string;
+export const COLOR_RED_24 : string;
+/** Rubin Red */
+export const COLOR_RED_39 : string;
+/** Contrast safer version of Rubin Red */
+export const COLOR_RED_47 : string;
+/** Rubin Light Teal */
+export const COLOR_TEAL_68 : string;
+/** Rubin Teal */
+export const COLOR_TEAL_72 : string;
+export const COLOR_TEAL_74 : string;
+/** Rubin Dark Teal */
+export const COLOR_TEAL_83 : string;
+/** Rubin Pale Teal */
+export const COLOR_TEAL_09 : string;
+/** Rubin Light Turquoise */
+export const COLOR_TURQUOISE_18 : string;
+/** Rubin Turquoise */
+export const COLOR_TURQUOISE_63 : string;
+export const COLOR_TURQUOISE_74 : string;
+export const COLOR_TURQUOISE_04 : string;
+export const COLOR_FONT_PRIMARY : string;
+export const COLOR_FONT_INVERT : string;
+export const COLOR_FONT_ACCENT : string;
+/** Rubin Yellow */
+export const COLOR_YELLOW_30 : string;
+export const COLOR_PALETTE_PRIMARY_GRAY_PALE : string;
+export const COLOR_PALETTE_PRIMARY_CHARCOAL : string;
+export const COLOR_PALETTE_PRIMARY_TEAL : string;
+export const COLOR_PALETTE_PRIMARY_TURQUOISE : string;
+export const COLOR_PALETTE_COMPLEMENTARY_GRAY_LIGHT : string;
+export const COLOR_PALETTE_COMPLEMENTARY_GRAY : string;
+export const COLOR_PALETTE_COMPLEMENTARY_BLACK : string;
+export const COLOR_PALETTE_COMPLEMENTARY_TEAL_PALE : string;
+export const COLOR_PALETTE_COMPLEMENTARY_TURQUOISE_LIGHT : string;
+export const COLOR_PALETTE_COMPLEMENTARY_TEAL_LIGHT : string;
+export const COLOR_PALETTE_COMPLEMENTARY_TEAL_DARK : string;
+export const COLOR_PALETTE_ACCENT_ORANGE : string;
+export const COLOR_PALETTE_ACCENT_BLUE : string;
+export const COLOR_PALETTE_ACCENT_GREEN : string;
+export const COLOR_PALETTE_ACCENT_RED : string;
+export const COLOR_PALETTE_ACCENT_YELLOW : string;
+export const COLOR_PALETTE_ACCENT_PURPLE : string;
+export const ELEVATION_ELEMENT_LOADER : number;
+export const ELEVATION_ELEMENT_DIALOG : number;
+export const ELEVATION_ELEMENT_HEADER : number;
+export const FONT_STACK_BASE : string;
+export const WEIGHT_FONT_NORMAL : string;
+export const WEIGHT_FONT_MEDIUM : string;
+export const WEIGHT_FONT_BOLD : string;
+export const WEIGHT_FONT_BLACK : string;
+export const SIZE_BREAKPOINT_DESKTOP : string;
+export const SIZE_BREAKPOINT_DESKTOP_SMALL : string;
+export const SIZE_BREAKPOINT_TABLET_LARGE : string;
+export const SIZE_BREAKPOINT_TABLET_LARGE_MIN : string;
+export const SIZE_BREAKPOINT_TABLET : string;
+export const SIZE_BREAKPOINT_TABLET_MIN : string;
+export const SIZE_BREAKPOINT_PHABLET : string;
+export const SIZE_BREAKPOINT_PHABLET_MIN : string;
+export const SIZE_BREAKPOINT_MOBILE : string;
+/** For extremely thin devices, or users with larger font bases */
+export const SIZE_BREAKPOINT_MOBILE_MIN : string;
+export const SIZE_CONTAINERS_MAX : string;
+export const SIZE_CONTAINERS_FULL : string;
+export const SIZE_CONTAINERS_WIDE : string;
+export const SIZE_CONTAINERS_REGULAR : string;
+export const SIZE_CONTAINERS_NARROW : string;
+export const SIZE_SPACING_XS : string;
+export const SIZE_SPACING_SM : string;
+export const SIZE_SPACING_MD : string;
+export const SIZE_SPACING_LG : string;
+export const SIZE_FONT_DESKTOP : string;
+export const SIZE_FONT_MOBILE : string;
+export const SIZE_FONT_HEADER_1 : string;
+export const SIZE_FONT_HEADER_2 : string;
+export const SIZE_FONT_HEADER_3 : string;
+export const SIZE_FONT_HEADER_4 : string;
+export const SIZE_LEADING_BASE : string;
+export const SIZE_LEADING_HEADER_1 : string;
+export const SIZE_LEADING_HEADER_2 : string;
+export const SIZE_LEADING_HEADER_3 : string;
+export const SIZE_LEADING_HEADER_4 : string;
+export const TIME_TRANSITION_BASE : string;
+export const TIME_TRANSITION_SLOW : string;
+export const TIME_TRANSITION_SLOWEST : string;
