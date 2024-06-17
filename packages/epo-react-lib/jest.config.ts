786fdb4f605296633edd687cad1e19b00d0cb2fc
Alexandra Goff <agoff@lsst.org>
2024-06-17 14:41:01 -0700

    [C] add CSS transform

--- packages/epo-react-lib/jest.config.ts
+++ packages/epo-react-lib/jest.config.ts
@@ -28,6 +28,7 @@ const config: JestConfigWithTsJest = {
     ...tsjPreset.transform,
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileTransformer.js",
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
   },
 };
 
