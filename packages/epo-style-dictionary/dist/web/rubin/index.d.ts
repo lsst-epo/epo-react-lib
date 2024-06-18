f611b09f29fefc438a49f316f1addbd0948c60fb
Alexandra Goff <agoff@lsst.org>
2024-06-18 14:06:03 -0700

    [C] map 2024 colors

--- packages/epo-style-dictionary/dist/web/rubin/index.d.ts
+++ packages/epo-style-dictionary/dist/web/rubin/index.d.ts
@@ -1,86 +1,77 @@
 /**
  * Do not edit directly
- * Generated on Fri, 14 Jun 2024 17:44:52 GMT
+ * Generated on Tue, 18 Jun 2024 21:03:20 GMT
  */
 
-export const COLOR_BLUE_13 : string;
-/** Rubin Blue */
-export const COLOR_BLUE_62 : string;
-export const COLOR_GREEN_14 : string;
-/** Rubin Green */
-export const COLOR_GREEN_54 : string;
-export const COLOR_NEUTRAL_10 : string;
-/** Rubin Light Gray */
-export const COLOR_NEUTRAL_12 : string;
-export const COLOR_NEUTRAL_17 : string;
-export const COLOR_NEUTRAL_37 : string;
-/** Rubin Gray */
-export const COLOR_NEUTRAL_58 : string;
-export const COLOR_NEUTRAL_75 : string;
-/** Rubin Charcoal */
-export const COLOR_NEUTRAL_80 : string;
-export const COLOR_NEUTRAL_81 : string;
-/** Rubin Black */
-export const COLOR_NEUTRAL_87 : string;
-export const COLOR_NEUTRAL_100 : string;
-export const COLOR_NEUTRAL_00 : string;
-export const COLOR_NEUTRAL_02 : string;
-/** Rubin Pale Gray */
-export const COLOR_NEUTRAL_04 : string;
-export const COLOR_NEUTRAL_08 : string;
-export const COLOR_NEUTRAL_09 : string;
-export const COLOR_ORANGE_10 : string;
-/** Rubin Orange */
-export const COLOR_ORANGE_31 : string;
-export const COLOR_ORANGE_01 : string;
-export const COLOR_ORANGE_03 : string;
-export const COLOR_ORANGE_07 : string;
-export const COLOR_BACKGROUND_PRIMARY : string;
-export const COLOR_BACKGROUND_INVERT : string;
-export const COLOR_PURPLE_67 : string;
-export const COLOR_RED_15 : string;
-export const COLOR_RED_24 : string;
-/** Rubin Red */
-export const COLOR_RED_39 : string;
-/** Contrast safer version of Rubin Red */
-export const COLOR_RED_47 : string;
-/** Rubin Light Teal */
-export const COLOR_TEAL_68 : string;
-/** Rubin Teal */
-export const COLOR_TEAL_72 : string;
-export const COLOR_TEAL_74 : string;
-/** Rubin Dark Teal */
-export const COLOR_TEAL_83 : string;
-/** Rubin Pale Teal */
-export const COLOR_TEAL_09 : string;
-/** Rubin Light Turquoise */
-export const COLOR_TURQUOISE_18 : string;
-/** Rubin Turquoise */
-export const COLOR_TURQUOISE_63 : string;
-export const COLOR_TURQUOISE_74 : string;
-export const COLOR_TURQUOISE_04 : string;
+export const COLOR_BACKGROUND_BUTTON_PRIMARY_DEFAULT : string;
+export const COLOR_BACKGROUND_BUTTON_PRIMARY_HOVER : string;
+export const COLOR_BACKGROUND_BUTTON_PRIMARY_INACTIVE : string;
+export const COLOR_BACKGROUND_BUTTON_SECONDARY_DEFAULT : string;
+export const COLOR_BACKGROUND_BUTTON_SECONDARY_HOVER : string;
+export const COLOR_BACKGROUND_BUTTON_SECONDARY_INACTIVE : string;
+export const COLOR_BACKGROUND_BUTTON_TERTIARY_DEFAULT : string;
+export const COLOR_BACKGROUND_BUTTON_TERTIARY_HOVER : string;
+export const COLOR_BACKGROUND_BUTTON_TERTIARY_INACTIVE : string;
+export const COLOR_BACKGROUND_BUTTON_EDUCATOR_DEFAULT : string;
+export const COLOR_BACKGROUND_BUTTON_EDUCATOR_HOVER : string;
+export const COLOR_BACKGROUND_BUTTON_EDUCATOR_INACTIVE : string;
+export const COLOR_BACKGROUND_BUTTON_CALLOUT_DEFAULT : string;
+export const COLOR_BACKGROUND_BUTTON_CALLOUT_HOVER : string;
+export const COLOR_BACKGROUND_BUTTON_CALLOUT_INACTIVE : string;
+export const COLOR_BACKGROUND_BUTTON_LINK_DEFAULT : string;
+export const COLOR_BACKGROUND_BUTTON_LINK_HOVER : string;
+export const COLOR_BACKGROUND_BUTTON_LINK_INACTIVE : string;
+export const COLOR_BACKGROUND_PAGE_PRIMARY : string;
+export const COLOR_BACKGROUND_PAGE_INVERT : string;
+export const COLOR_BORDER_BUTTON_PRIMARY : string;
+export const COLOR_BORDER_BUTTON_SECONDARY : string;
+export const COLOR_BORDER_BUTTON_TERTIARY : string;
+export const COLOR_BORDER_BUTTON_EDUCATOR : string;
+export const COLOR_BORDER_BUTTON_CALLOUT : string;
+export const COLOR_BORDER_BUTTON_LINK : string;
+export const COLOR_OUTLINE_BUTTON_PRIMARY : string;
+export const COLOR_OUTLINE_BUTTON_SECONDARY : string;
+export const COLOR_OUTLINE_BUTTON_TERTIARY : string;
+export const COLOR_OUTLINE_BUTTON_EDUCATOR : string;
+export const COLOR_OUTLINE_BUTTON_CALLOUT : string;
+export const COLOR_OUTLINE_BUTTON_LINK : string;
+export const COLOR_TEXT_BUTTON_PRIMARY : string;
+export const COLOR_TEXT_BUTTON_SECONDARY : string;
+export const COLOR_TEXT_BUTTON_TERTIARY : string;
+export const COLOR_TEXT_BUTTON_EDUCATOR : string;
+export const COLOR_TEXT_BUTTON_CALLOUT : string;
+export const COLOR_TEXT_BUTTON_LINK : string;
+export const COLOR_RUBIN_TEAL_100 : string;
+export const COLOR_RUBIN_TEAL_200 : string;
+export const COLOR_RUBIN_TEAL_300 : string;
+export const COLOR_RUBIN_TEAL_400 : string;
+export const COLOR_RUBIN_TEAL_500 : string;
+export const COLOR_RUBIN_TEAL_600 : string;
+export const COLOR_RUBIN_TEAL_700 : string;
+export const COLOR_RUBIN_GRAY_100 : string;
+export const COLOR_RUBIN_GRAY_200 : string;
+export const COLOR_RUBIN_GRAY_300 : string;
+export const COLOR_RUBIN_RED_100 : string;
+export const COLOR_RUBIN_RED_200 : string;
+export const COLOR_RUBIN_RED_300 : string;
+export const COLOR_RUBIN_ORANGE_100 : string;
+export const COLOR_RUBIN_ORANGE_200 : string;
+export const COLOR_RUBIN_ORANGE_300 : string;
+export const COLOR_RUBIN_YELLOW_100 : string;
+export const COLOR_RUBIN_YELLOW_200 : string;
+export const COLOR_RUBIN_YELLOW_300 : string;
+export const COLOR_RUBIN_GREEN_100 : string;
+export const COLOR_RUBIN_GREEN_200 : string;
+export const COLOR_RUBIN_GREEN_300 : string;
+export const COLOR_RUBIN_BLUE_100 : string;
+export const COLOR_RUBIN_BLUE_200 : string;
+export const COLOR_RUBIN_BLUE_300 : string;
+export const COLOR_RUBIN_PURPLE_100 : string;
+export const COLOR_RUBIN_PURPLE_200 : string;
+export const COLOR_RUBIN_PURPLE_300 : string;
 export const COLOR_FONT_PRIMARY : string;
 export const COLOR_FONT_INVERT : string;
 export const COLOR_FONT_ACCENT : string;
-/** Rubin Yellow */
-export const COLOR_YELLOW_30 : string;
-export const COLOR_PALETTE_PRIMARY_GRAY_PALE : string;
-export const COLOR_PALETTE_PRIMARY_CHARCOAL : string;
-export const COLOR_PALETTE_PRIMARY_TEAL : string;
-export const COLOR_PALETTE_PRIMARY_TURQUOISE : string;
-export const COLOR_PALETTE_COMPLEMENTARY_GRAY_LIGHT : string;
-export const COLOR_PALETTE_COMPLEMENTARY_GRAY : string;
-export const COLOR_PALETTE_COMPLEMENTARY_BLACK : string;
-export const COLOR_PALETTE_COMPLEMENTARY_TEAL_PALE : string;
-export const COLOR_PALETTE_COMPLEMENTARY_TURQUOISE_LIGHT : string;
-export const COLOR_PALETTE_COMPLEMENTARY_TEAL_LIGHT : string;
-export const COLOR_PALETTE_COMPLEMENTARY_TEAL_DARK : string;
-export const COLOR_PALETTE_ACCENT_ORANGE : string;
-export const COLOR_PALETTE_ACCENT_BLUE : string;
-export const COLOR_PALETTE_ACCENT_GREEN : string;
-export const COLOR_PALETTE_ACCENT_RED : string;
-export const COLOR_PALETTE_ACCENT_YELLOW : string;
-export const COLOR_PALETTE_ACCENT_PURPLE : string;
 export const ELEVATION_ELEMENT_LOADER : number;
 export const ELEVATION_ELEMENT_DIALOG : number;
 export const ELEVATION_ELEMENT_HEADER : number;
