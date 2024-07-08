d225621688f668ad3ffc971e0f8de7273ff3f479
Alexandra Goff <agoff@lsst.org>
2024-07-08 15:51:08 -0700

    [C] fix tests

--- packages/epo-widget-lib/src/widgets/SourceSelector/SourceSelector.test.tsx
+++ packages/epo-widget-lib/src/widgets/SourceSelector/SourceSelector.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SourceSelector from ".";
 import { biggerData } from "./mocks";
+import { toDecimalPercent } from "./utils";
 
 const selectionCallback = jest.fn();
 const alertChangeCallback = jest.fn();
@@ -8,26 +10,40 @@ const { sources, alerts } = biggerData;
 const props = { sources, alerts, selectionCallback, alertChangeCallback };
 
 describe("SourceSelector", () => {
-  it("should render sources into an SVG", () => {
-    render(<SourceSelector {...props} />);
-    const points = screen.getAllByRole("listitem");
+  it("should select a source when its point is clicked", async () => {
+    const { getByTestId, getByRole } = render(<SourceSelector {...props} />);
+    const target = sources[0];
+    const blinker = getByTestId("blinker-container");
+    const { width, height } = blinker.getBoundingClientRect();
 
-    expect(points.length).toBe(sources.length);
-  });
-  it("should select a source when its point is clicked", () => {
-    render(<SourceSelector {...props} />);
-    const points = screen.getAllByRole("listitem");
-    fireEvent.click(points[0]);
+    await userEvent.pointer({
+      keys: "[MouseLeft]",
+      target: blinker,
+      coords: {
+        x: width * toDecimalPercent(target.x),
+        y: height * toDecimalPercent(target.x),
+      },
+    });
+    const points = getByRole("list");
 
     waitFor(() => {
-      expect(selectionCallback).toBeCalledWith(sources[0]);
+      expect(selectionCallback).toBeCalledWith([target.id]);
+      expect(points.children.length).toBe(1);
     });
   });
-  it("should not make a selection when another SVG element is clicked", () => {
-    render(<SourceSelector {...props} />);
+  it("should not make a selection when an area outside a point is clicked", async () => {
     jest.clearAllMocks();
-    const group = screen.getByRole("list");
-    fireEvent.click(group);
+    const { getByTestId, getByRole } = render(<SourceSelector {...props} />);
+    const blinker = getByTestId("blinker-container");
+
+    await userEvent.pointer({
+      keys: "[MouseLeft]",
+      target: blinker,
+      coords: {
+        x: 0,
+        y: 0,
+      },
+    });
 
     expect(selectionCallback).not.toBeCalled();
   });
