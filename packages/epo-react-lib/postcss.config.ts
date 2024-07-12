4fa5dd8149baebc11bb5b815b186171b8a41012c
Alexandra Goff <agoff@lsst.org>
2024-07-12 11:20:53 -0700

    [C] add postCSS normalize

--- packages/epo-react-lib/postcss.config.ts
+++ packages/epo-react-lib/postcss.config.ts
@@ -1,6 +1,11 @@
-import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
+import normalize from "postcss-normalize";
+import preset from "postcss-preset-env";
 
 export default {
-  plugins: [autoprefixer(), cssnano({ preset: "default" })],
+  plugins: [
+    preset(),
+    normalize({ forceImport: true }),
+    cssnano({ preset: "default" }),
+  ],
 };
