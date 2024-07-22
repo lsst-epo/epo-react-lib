e5208c31730383fc9d070daa040dbff825b91991
Alexandra Goff <agoff@lsst.org>
2024-07-22 16:03:01 -0700

    build(repo): add all packages as scope names

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-export default { extends: ["@commitlint/config-conventional", "monorepo"] };
+import { readdirSync } from "fs";
+const DEFAULT_SCOPES = ["repo"];
+
+const packageDirNames = readdirSync("./packages", { withFileTypes: true })
+  .filter((entry) => entry.isDirectory())
+  .map((dir) => dir.name);
+
+const scopes = DEFAULT_SCOPES.concat(packageDirNames);
+
+export default {
+  extends: ["@commitlint/config-conventional", "monorepo"],
+  rules: {
+    "scope-enum": [2, "always", scopes],
+  },
+};
