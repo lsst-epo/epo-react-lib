0e9146e039d90757dd61f8e1fb941473c19e47e1
Alexandra Goff <agoff@lsst.org>
2024-08-22 16:22:53 -0700

    ci(repo): change commit-analyzer preset

--- release.config.js
+++ release.config.js
@@ -3,6 +3,14 @@
  */
 const release = {
   branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+  ],
 };
 
 export default release;
