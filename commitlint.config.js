69308da8cab1d22da78099ff3b6fdd639cc2a37f
Alexandra Goff <agoff@lsst.org>
2024-08-27 09:18:39 -0700

    ci: remove monorepo commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,3 @@
-import { readdirSync } from "fs";
-const DEFAULT_SCOPES = ["repo"];
-
-const packageDirNames = readdirSync("./packages", { withFileTypes: true })
-  .filter((entry) => entry.isDirectory())
-  .map((dir) => dir.name);
-
-const scopes = DEFAULT_SCOPES.concat(packageDirNames);
-
 export default {
-  extends: ["@commitlint/config-conventional", "monorepo"],
-  rules: {
-    "scope-enum": [2, "always", scopes],
-  },
+  extends: ["@commitlint/config-conventional"],
 };
