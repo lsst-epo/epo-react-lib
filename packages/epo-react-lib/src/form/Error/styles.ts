02d7f1ac313982599a86011cd27f26224d9d96d7
Alexandra Goff <agoff@lsst.org>
2024-09-04 11:16:24 -0700

    feat: accessible error message

--- packages/epo-react-lib/src/form/Error/styles.ts
+++ packages/epo-react-lib/src/form/Error/styles.ts
@@ -1,9 +1,6 @@
 import styled from "styled-components";
-import { ScreenreaderText } from "@/styles/utils";
 
 export const Error = styled.span`
   color: var(--color-rubin-red-300);
   font-weight: var(--weight-font-normal);
 `;
-
-export const ErrorLabel = ScreenreaderText;
