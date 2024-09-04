d3f045e9b49122d7cd21849eb32650eaf8a26f9d
Alexandra Goff <agoff@lsst.org>
2024-09-04 12:59:34 -0700

    fix: add size fallbacks

--- packages/epo-react-lib/src/form/Input/styles.ts
+++ packages/epo-react-lib/src/form/Input/styles.ts
@@ -5,13 +5,13 @@ export const Input = styled.input`
   --color-input-border-hover: var(--color-font-accent);
 
   border: 1px solid var(--color-input-border);
-  border-radius: var(--size-spacing-3xs);
+  border-radius: var(--size-spacing-3xs, calc(1em * 0.25));
   box-sizing: border-box;
   font-weight: var(--weight-font-normal);
-  height: var(--size-spacing-xl);
+  height: var(--size-spacing-xl, calc(1em * 3));
   line-height: 1;
   width: 100%;
-  padding: var(--size-spacing-s);
+  padding: var(--size-spacing-s, 1em);
 
   &:not(:disabled):hover,
   &:not(:disabled):focus-within {
