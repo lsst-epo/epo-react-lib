54265dcb0c2463bfe8f619ca1a71c6390881bd42
Sasha Persaud <sasharavpersaud@gmail.com>
2025-11-03 12:22:08 -0700

    feat: converted main OrbitalViewer SASS styles to CSS

--- packages/epo-widget-lib/src/widgets/OrbitalSim/OrbitalSim.stories.tsx
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/OrbitalSim.stories.tsx
@@ -1,13 +1,14 @@
 import { Meta, StoryFn } from "@storybook/react";
+import OrbitalSim from ".";
 
-const meta: Meta<typeof OrbitalViewer> = {
-    component: OrbitalViewer,
+const meta: Meta<typeof OrbitalSim> = {
+  component: OrbitalSim,
 };
 export default meta;
 
 // Template for all stories
-const Template: StoryFn<typeof OrbitalViewer> = (args) => {
-    return <OrbitalViewer {...args}></OrbitalViewer>
+const Template: StoryFn<typeof OrbitalSim> = (args) => {
+  return <h1></h1>;
 };
 
 
