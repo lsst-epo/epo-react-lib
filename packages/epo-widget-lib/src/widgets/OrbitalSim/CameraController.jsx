96f9e5bfab87ee9e36406a1f025f4526da0133c6
Sasha Persaud <sasharavpersaud@gmail.com>
2025-11-03 14:24:23 -0700

    feat: added CameraController component and props to the OrbitalSim Storybook config

---
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/CameraController.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { useThree } from 'react-three-fiber';
+
+function CameraController({ pov, reset }) {
+  const { camera, gl } = useThree();
+  const orbitControls = useRef();
+
+  function setPov() {
+    if (pov) {
+      const targetPov = {
+        side: Math.PI / 1.0075,
+        top: Math.PI / 2,
+      }[pov];
+      // horizontal rotation
+      orbitControls.current.minAzimuthAngle = 0;
+      orbitControls.current.maxAzimuthAngle = 0;
+      // vertical rotation
+      orbitControls.current.minPolarAngle = targetPov;
+      orbitControls.current.maxPolarAngle = targetPov;
+    }
+
+    orbitControls.current.update();
+  }
+
+  useEffect(() => {
+    if (orbitControls.current && reset > 0) {
+      orbitControls.current.reset();
+    }
+  }, [reset]);
+
+  useEffect(() => {
+    orbitControls.current = new OrbitControls(camera, gl.domElement);
+    orbitControls.current.minDistance = 70;
+    orbitControls.current.maxDistance = 10000;
+    orbitControls.current.screenSpacePanning = true;
+    // controls.mouseButtons.RIGHT = THREE.MOUSE.PAN;
+    // controls.mouseButtons.LEFT = THREE.MOUSE.ROTATE;
+    // controls.touches.TWO = THREE.TOUCH.PAN;
+    // controls.touches.ONE = THREE.TOUCH.DOLLY_ROTATE;
+
+    setPov();
+    return () => {
+      orbitControls.current.dispose();
+    };
+  }, [camera, gl]);
+
+  return null;
+}
+
+CameraController.propTypes = {
+  pov: PropTypes.string,
+  reset: PropTypes.number,
+};
+
+export default CameraController;
--- packages/epo-widget-lib/src/widgets/OrbitalSim/OrbitalSim.stories.tsx
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/OrbitalSim.stories.tsx
@@ -8,12 +8,96 @@ export default meta;
 
 // Template for all stories
 const Template: StoryFn<typeof OrbitalSim> = (args) => {
-  return <OrbitalSim></OrbitalSim>;
+  return <OrbitalSim {...args} />;
 };
-
 
 // Default story
 export const Primary = Template.bind({});
-Primary.args = {};
+// Taken from Has Ast pg /close-to-earth-1/
+Primary.args = {
+  neos: [
+    {
+      NEO_flag: 1,
+      One_km_NEO_flag: 1,
+      H: 17.7,
+      G: 0.15,
+      Num_obs: 430,
+      rms: 1.15,
+      U: "0",
+      Arc_years: "1932-2018",
+      Perturbers: "M-v",
+      Perturbers_2: "3Eh",
+      Number: "(1221)",
+      Name: "Amor",
+      Principal_desig: "1932 EA1",
+      Epoch: 2458600.5,
+      M: 250.28017,
+      Peri: 26.67495,
+      Node: 171.33714,
+      i: 11.87658,
+      e: 0.4353207,
+      n: 0.37063846,
+      a: 1.9194158,
+      Ref: "MPO458839",
+      Num_opps: 20,
+      Computer: "MPCW",
+      Hex_flags: "1804",
+      Last_obs: "2018-10-15",
+      Tp: 2458896.52931,
+      Orbital_period: 2.6592159,
+      Perihelion_dist: 1.0838544,
+      Aphelion_dist: 2.7549772,
+      Semilatus_rectum: 0.7778393,
+      Synodic_period: 1.6026943,
+      Orbit_type: "Amor",
+    },
+  ],
+  activeNeo: {
+    NEO_flag: 1,
+    One_km_NEO_flag: 1,
+    H: 17.7,
+    G: 0.15,
+    Num_obs: 430,
+    rms: 1.15,
+    U: "0",
+    Arc_years: "1932-2018",
+    Perturbers: "M-v",
+    Perturbers_2: "3Eh",
+    Number: "(1221)",
+    Name: "Amor",
+    Principal_desig: "1932 EA1",
+    Epoch: 2458600.5,
+    M: 250.28017,
+    Peri: 26.67495,
+    Node: 171.33714,
+    i: 11.87658,
+    e: 0.4353207,
+    n: 0.37063846,
+    a: 1.9194158,
+    Ref: "MPO458839",
+    Num_opps: 20,
+    Computer: "MPCW",
+    Hex_flags: "1804",
+    Last_obs: "2018-10-15",
+    Tp: 2458896.52931,
+    Orbital_period: 2.6592159,
+    Perihelion_dist: 1.0838544,
+    Aphelion_dist: 2.7549772,
+    Semilatus_rectum: 0.7778393,
+    Synodic_period: 1.6026943,
+    Orbit_type: "Amor",
+  },
+  activeObs: undefined,
+  selectionCallback: () => {},
+  paused: true,
+  pov: "top",
+  defaultZoom: null,
+  potentialOrbits: null,
+  observations: null,
+  noDetails: true,
+  detailsSet: null,
+  refObjs: ["earth"],
+  noLabels: null,
+};
 
 // Other Stories
