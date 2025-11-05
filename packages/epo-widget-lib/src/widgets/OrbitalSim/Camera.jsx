cd909b9e6ad093e6f3b65563b6bdff7e759b8d00
Sasha Persaud <sasharavpersaud@gmail.com>
2025-11-05 16:05:25 -0700

    feat: added minimum translation support for Sun component

--- packages/epo-widget-lib/src/widgets/OrbitalSim/Camera.jsx
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/Camera.jsx
@@ -1,17 +1,14 @@
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useFrame, useThree } from "@react-three/fiber";
-// import { PerspectiveCamera } from '@react-three/drei'
 
 function Camera({ ...props }) {
   const ref = useRef();
-  // const { setDefaultCamera } = useThree();
   // Make the camera known to the system
   useEffect(() => {
     const $el = ref.current;
     $el.zoom = props.defaultZoom;
     $el.updateProjectionMatrix();
-    // setDefaultCamera($el);
   }, []);
 
   // Update it every frame
