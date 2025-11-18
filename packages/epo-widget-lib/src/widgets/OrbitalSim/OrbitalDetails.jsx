2a5662e8228a0727386fc74f744cda8be8c3526d
Sasha Persaud <sasharavpersaud@gmail.com>
2025-11-17 20:57:28 -0700

    fix: fixed button overlay styling bug in OrbitalDetails

--- packages/epo-widget-lib/src/widgets/OrbitalSim/OrbitalDetails.jsx
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/OrbitalDetails.jsx
@@ -1,90 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useTranslation } from "react-i18next";
-import classnames from 'classnames';
-import styles from './OrbitalSim.module.css';
-import Unit from '../../charts/Unit/index.jsx';
-
-// import Table from '../../site/forms/table/index.jsx';
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import styles from "./OrbitalSim.module.css";
+import Table from "@rubin-epo/epo-react-lib/SimpleTable";
 import Button from "@rubin-epo/epo-react-lib/Button";
 
-
 // TODO: Revisit file organization to improve this import
-import { getValue, toSigFigs } from '../../../../epo-react-lib/src/lib/calculations.js';
-
-function OrbitalDetails({ data, velocity, type }) {
-  const { t } = useTranslation('widgets');
-  const { H, a, i, e, name, Principal_desig: pd, Earth_moid: moid } =
-    data || {};
+function OrbitalDetails({ rows, velocity, type }) {
   const [active, setActive] = useState(false);
 
   useEffect(() => {
-    if (data) setActive(true);
-  }, [data]);
-
-  function renderValueWithUnits(value, unitType, showUnit) {
-    if (!value) return '';
-
-    return (
-      <>
-        {getValue(unitType, value)}
-        {showUnit && <Unit type={unitType} />}
-      </>
-    );
-  }
-
-  function getRows() {
-    if (type === 'hazardous-asteroids') {
-      return [
-        [t('orbit_viewer.details_table.scientific_name'), name || pd || ''],
-        [
-          t('orbit_viewer.details_table.absolute_magnitude'),
-          renderValueWithUnits(H, 'magnitude', false),
-        ],
-        [
-          t('orbit_viewer.details_table.earth_moid'),
-          renderValueWithUnits(moid, 'moid', true),
-        ],
-      ];
-    }
-
-    return [
-      [t('orbit_viewer.details_table.scientific_name'), name || pd || ''],
-      [
-        t('orbit_viewer.details_table.orbit_size'),
-        renderValueWithUnits(a, 'semimajor_axis', true),
-      ],
-      [
-        t('orbit_viewer.details_table.eccentricity'),
-        renderValueWithUnits(e, 'eccentricity', false),
-      ],
-      [
-        t('orbit_viewer.details_table.inclination'),
-        renderValueWithUnits(i, 'inclination', true),
-      ],
-      [
-        t('orbit_viewer.details_table.orbital_speed'),
-        renderValueWithUnits(toSigFigs(velocity, 3), 'velocity', true),
-      ],
-    ];
-  }
+    if (rows) setActive(true);
+  }, [rows]);
 
   return (
     <>
       <Button
         styleAs="secondary"
-        isInactive={!data}
+        className={styles["details-toggle"]}
+        isInactive={rows}
         onClick={() => setActive(!active)}
       >
-        {t("orbit_viewer.details", { context: active ? "hide" : "show" })}
+        {active ? "Hide Details" : "Show Details"}
       </Button>
-      {/* <div
+      <div
         className={classnames(styles.details, {
           [styles.activeDetails]: active,
         })}
       >
-        <Table className={styles.detailsTable} includeRowTitles rows={getRows()} />
-      </div> */}
+        {/* <Table className={styles.detailsTable} rows={rows} /> */}
+        <Table rows={rows} />
+      </div>
     </>
   );
 }
