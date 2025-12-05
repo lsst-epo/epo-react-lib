e330ccd5cfe1074e248211b7e3cc938e048eb31d
Sasha Persaud <sasharavpersaud@gmail.com>
2025-12-05 12:04:52 -0700

    feat: added slider to OrbitalSim, needs styling touchups

---
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/PlaybackSpeed.jsx
@@ -0,0 +1,142 @@
+/* eslint-disable react/no-danger */
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from "react-i18next";
+// TODO: Revisit this import to figure out how to do this with aliases
+import {formatValue} from "../../../../epo-react-lib/src/lib/calculations"; 
+import HorizontalSlider from '@rubin-epo/epo-react-lib/HorizontalSlider';
+import styles from "./OrbitalSim.module.css";
+
+
+function PlaybackSpeed({
+  dayPerVizSec,
+  elapsedTime,
+  sliderOnChangeCallback,
+  speeds,
+}) {
+  const { t } = useTranslation('widgets');
+
+  function formatSpeed(speed) {
+    const years = speed / 365;
+    const justYears = Math.floor(years);
+    const justDays = 365 * (years - justYears);
+    let formattedYears = {};
+    let formattedDays = {};
+
+    formattedYears = {
+      number: formatValue(justYears),
+      string: t('elapsed_time.interval.year', { count: justYears }),
+    };
+
+    formattedDays = {
+      number: formatValue(justDays),
+      string: t('elapsed_time.interval.day', { count: justDays }),
+    };
+
+    if (justYears >= 1) {
+      return `${formattedYears.number} ${formattedYears.string}`;
+    }
+
+    if (justDays < 1) {
+      return t('orbit_viewer.playback.interval.secWithCount', { count: 1 });
+    }
+
+    return `${formattedDays.number} ${formattedDays.string}`;
+  }
+
+  function formatElapsed(type, value) {
+    const isNegative = elapsedTime < 0;
+    const years = Math.abs(elapsedTime) / 365.256;
+    const justYears = Math.floor(years);
+    const justDays = Math.floor(365.256 * (years - justYears));
+    let formattedYears = {};
+    let formattedDays = {};
+
+    formattedYears = {
+      number: formatValue(isNegative ? -justYears : justYears),
+      string: t('elapsed_time.interval.year', { count: justYears }),
+    };
+
+    formattedDays = {
+      number: formatValue(isNegative ? -justDays : justDays),
+      string: t('elapsed_time.interval.day', { count: justDays }),
+    };
+
+    if (type === 'days') return formattedDays[value];
+    if (type === 'years') return formattedYears[value];
+
+    return null;
+  }
+
+  const formattedSpeed = useMemo(() => {
+    return formatSpeed(dayPerVizSec);
+  }, [dayPerVizSec]);
+
+  return (
+    <>
+      <div className={styles["playback-speed-slider-header"]}>
+        <h4 className={styles["playback-speed-title"]}>
+          {t('orbit_viewer.playback.time_step')}
+        </h4>
+        <div className={styles["playback-speed-slider-label"]}>
+          {t('orbit_viewer.playback.time_equivalence', {
+            firstTime: t('orbit_viewer.playback.interval.secWithCount', {
+              count: 1,
+            }),
+            secondTime: formattedSpeed,
+          })}
+        </div>
+      </div>
+      <div className={styles["playback-speed-slider-label-top"]}>
+        {t('orbit_viewer.playback.time_equivalence', {
+          firstTime: t('orbit_viewer.playback.interval.secWithCount', {
+            count: 1,
+          }),
+          secondTime: t('orbit_viewer.playback.interval.yearWithCount', {
+            count: 1,
+          }),
+          context: 'verbose',
+        })}
+      </div>
+      <div className={styles["playback-speed-slider-label-bottom"]}>
+        {t('orbit_viewer.playback.normal_time')}
+      </div>
+      <HorizontalSlider
+        className={styles["playback-speed-slider"]}
+        isVertical={true}
+        min={speeds.min}
+        max={speeds.max}
+        step={speeds.step}
+        value={dayPerVizSec}
+        onChangeCallback={sliderOnChangeCallback}
+      />
+      <div className={styles["elapsed-time-container"]}>
+        <div className={styles["elapsed-time-title"]}>{t('elapsed_time.title')}</div>
+        <div className={styles["elapsed-time-inner"]}>
+          <div className={styles["elapsed-time-block"]}>
+            <div className={styles["elapsed-val"]}>{formatElapsed('years', 'number')}</div>
+            <div className={styles["elapsed-time-label"]}>
+              {formatElapsed('years', 'string')}
+            </div>
+          </div>
+          <div className={styles["elapsed-time-divider"]}></div>
+          <div className={styles["elapsed-time-block"]}>
+            <div className={styles["elapsed-val"]}>{formatElapsed('days', 'number')}</div>
+            <div className={styles["elapsed-label"]}>
+              {formatElapsed('days', 'string')}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+PlaybackSpeed.propTypes = {
+  dayPerVizSec: PropTypes.number,
+  elapsedTime: PropTypes.number,
+  speeds: PropTypes.object,
+  sliderOnChangeCallback: PropTypes.func,
+};
+
+export default PlaybackSpeed;
