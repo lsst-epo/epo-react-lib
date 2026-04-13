9c4aae0d2a1e6a4f410f2977ba6dcb9e90f523fa
Jeff Innes <jeff.innes@noirlab.edu>
2026-04-13 15:23:07 -0700

    refactor: relocate SelectionList component to atomic components directory and update import paths

--- packages/epo-widget-lib/src/widgets/SourceSelector/index.tsx
+++ packages/epo-widget-lib/src/widgets/SourceSelector/index.tsx
@@ -1,4 +1,4 @@
 import SourceSelector from "./SourceSelector";
 export { default as Message } from "./Message";
-export { default as SelectionList } from "./SelectionList";
+export { default as SelectionList } from "@/atomic/SelectionList"; //! Removing this will be a breaking change in the investigations client
 export default SourceSelector;
