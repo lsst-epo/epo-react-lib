95bc3fdcaa3d3e5edf3b373c20f60ebc93f442c2
Eric Rosas <ericdrosas@gmail.com>
2026-04-29 11:45:53 -0700

    fix: removed unnecessary camera configs

--- packages/epo-widget-lib/src/widgets/OrbitalSim/Camera/Camera.jsx
+++ packages/epo-widget-lib/src/widgets/OrbitalSim/Camera/Camera.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useFrame, useThree } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 import { OrthographicCamera } from '@react-three/drei';
 
 function Camera({ ...props }) {
