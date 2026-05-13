69b08be6b47d4df2c536aee63e39203339862ce1
Eric Rosas <ericdrosas@gmail.com>
2026-05-13 16:14:08 -0700

    feat: added `asteroid` to locale string and type

--- packages/epo-widget-lib/src/types/astro.d.ts
+++ packages/epo-widget-lib/src/types/astro.d.ts
@@ -1,5 +1,5 @@
 export type Band = "u" | "g" | "r" | "i" | "z" | "y";
-export type SourceType = "supernova" | "galaxy" | "galaxyFilter" | "observation";
+export type SourceType = "supernova" | "galaxy" | "galaxyFilter" | "observation | asteroid";
 
 export interface Source {
   id: string;
